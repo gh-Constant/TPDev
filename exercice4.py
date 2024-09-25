2a41d4fc486732f29ea3968e203aee7e01d3e54b
Constant <constantsuchet@gmail.com>
2024-09-25 16:22:35 +0200

    oui

---
+++ exercice4.py
@@ -0,0 +1,9 @@
+liste = [1,2,3]
+
+searched = 1
+
+for i in liste:
+    if i == searched:
+        print("Element found")
+        break
+
---
+++ exercice5.py
@@ -0,0 +1,6 @@
+binaire = [0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010, 1011, 1100, 1101, 1110, 1111]
+hexa = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+
+searched = input("Enter a binary number: ")
+
+print("The hexadecimal number is: ", hexa[binaire.index(int(searched))])
